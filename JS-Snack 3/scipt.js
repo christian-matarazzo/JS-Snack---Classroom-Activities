f3277a724e3cd131bc4ab2a05643612158dca45e
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-05 12:29:49 +0100

    add polishing

--- JS-Snack 3/scipt.js
+++ JS-Snack 3/scipt.js
@@ -6,17 +6,17 @@
 let somma = 0
 
 
-for(let i = 0; i < 10; i++)
+for(let i = 0; i < 10; i++) {
 
-    if ( i <= 10) {
 
-        numero = Number(prompt(`inserisci il numero ${i + 1} di 10`))
+       let numero = Number(prompt(`inserisci il numero ${i + 1} di 10`))
 
         /* do the sum */
 
         somma += numero
 
-    }
+}
+
 
     console.log(`La somma totale di tutti i numeri inseriti è : ${somma}`);
     

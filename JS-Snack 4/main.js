7d96a55d13902c325d257b1fbe84e065fa0c377d
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-05 12:55:40 +0100

    add comment

--- JS-Snack 4/main.js
+++ JS-Snack 4/main.js
@@ -1,13 +1,13 @@
-/* create an empty array for  */
+/* create an empty array for oddNumbers  */
 
 let oddNumbers = []
 
 for(let i = 0; i < 6 ; i++) {
-
+/* ask 6 numbers */
     let numbers = prompt(`Inserisci il valore numero ${i + 1} di 6`)
    
     console.log(numbers);
-    
+    /* if they are odd push in the array */
     if (numbers % 2 !== 0) {
 
         oddNumbers.push(numbers)
@@ -15,6 +15,6 @@ for(let i = 0; i < 6 ; i++) {
     
     
 }
-
+/* log the array */
 console.log(oddNumbers);
 

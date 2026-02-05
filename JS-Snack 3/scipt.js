d08c2cfa93f68350f5e6f4de068195e8ca980a95
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-05 11:57:09 +0100

    first commit

---
+++ JS-Snack 3/scipt.js
@@ -0,0 +1,23 @@
+/* ask to the user FOR 10 times 1 number*/
+
+
+
+
+let somma = 0
+let numero 
+
+for(i = 0; i < 10; i++)
+
+    if ( i <= 10) {
+
+        numero = Number(prompt(`inserisci il numero ${i + 1} di 10`))
+
+        /* do the sum */
+
+        somma += numero
+
+    }
+
+    console.log(`La somma totale di tutti i numeri inseriti è : ${somma}`);
+    
+

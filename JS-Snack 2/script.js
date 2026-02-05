fd8a6212ab91b0e0424a83057762a7ba4e21d116
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-05 11:41:36 +0100

    add fix

--- JS-Snack 2/script.js
+++ JS-Snack 2/script.js
@@ -1,8 +1,8 @@
 /* Ask two word to the user with prompt*/
 
-const first_Word = prompt("Please write your message here, it must be the shortest...")
+const first_Word = prompt("Please write your message here")
 
-const second_Word = prompt("Please write your second message here, it must be the longest...")
+const second_Word = prompt("Please write your second message here")
 
 /* console log the shortest one, and then the longhest */
 if (first_Word.length<second_Word.length) {
@@ -14,9 +14,27 @@ if (first_Word.length<second_Word.length) {
     
 }
 
-else { 
-  console.log("Sorry, you must respect the writing");
-  
+else if(first_Word.length>second_Word.length) {
+
+    console.log(second_Word);
+    console.log(first_Word);
     
+    
+    
+
     
 }
+
+else {
+
+    first_Word.length===second_Word.length
+
+    console.log('The words have same length');
+    
+
+
+}
+
+
+
+  
